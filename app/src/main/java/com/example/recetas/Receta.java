ac3f5158181824e712c894b336fd519e65dd2e97
Joebeltran <jabn1994_18@yahoo.es>
2019-08-01 15:12:40 -0600

    Diseño de Interfaz

---
+++ app/src/main/java/com/example/recetas/Receta.java
@@ -0,0 +1,27 @@
+package com.example.recetas;
+
+public class Receta {
+    private String titulo;
+    private String texto;
+
+    public Receta(String titulo, String texto) {
+        this.titulo = titulo;
+        this.texto = texto;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
